ea6f2d1a00cfb10c71fdcc9819004a602f8b7aa3
smmathen <70829009+smmathen@users.noreply.github.com>
2022-01-29 15:53:26 -0600

    First commit

---
+++ crypto-calc/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" height="100" >
+  <AppName msg="CryptoCalc"/>
+</template>
+
+
+<script>
+import AppName from './components/AppName.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppName
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

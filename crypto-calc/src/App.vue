cb5f60204af3109d167844fe912a41dfdaf72e16
smmathen <70829009+smmathen@users.noreply.github.com>
2022-01-29 21:51:30 -0600

    Added box and centered

--- crypto-calc/src/App.vue
+++ crypto-calc/src/App.vue
@@ -1,19 +1,28 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" height="100" >
   <AppName msg="CryptoCalc"/>
+  <div style="display: flex; justify-content: center;">
+    <Floater/>
+  </div>
 </template>
 
 
 <script>
 import AppName from './components/AppName.vue'
+import Floater from './components/Floater.vue'
+
+
 
 export default {
   name: 'App',
   components: {
-    AppName
-  }
+    AppName,
+    Floater
 }
+  }
+  
 </script>
+
 
 <style>
 #app {
---
+++ crypto-calc/src/components/Floater.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="mydiv">
+  <!-- Include a header DIV with the same name as the draggable DIV, followed by "header" -->
+  <div class="mydivheader" @mousedown="dragMouseDown">Cryptography Solver</div>
+  <div class="mainbox"></div> 
+</div>
+</template>
+
+<script>
+export default {
+    data: ()=>({
+        pos1: 0,
+        pos2: 0,
+        pos3: 0,
+        pos4: 0
+    }),
+
+    methods: {
+        dragMouseDown: function(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // get the mouse cursor position at startup:
+            this.pos3 = e.clientX;
+            this.pos4 = e.clientY;
+            document.onmouseup = this.closeDragElement;
+            // call a function whenever the cursor moves:
+            document.onmousemove = this.elementDrag;
+        },
+
+        elementDrag: function(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // calculate the new cursor position:
+            this.pos1 = this.pos3 - e.clientX;
+            this.pos2 = this.pos4 - e.clientY;
+            this.pos3 = e.clientX;
+            this.pos4 = e.clientY;
+            // set the element's new position:
+            this.$el.style.top = (this.$el.offsetTop - this.pos2) + "px";
+            this.$el.style.left = (this.$el.offsetLeft - this.pos1) + "px";
+        },
+
+        closeDragElement: function () {
+            document.onmouseup = null;
+            document.onmousemove = null;
+        }
+    }
+
+
+}
+</script>
+
+<style>
+    .mydiv {
+  position: absolute;
+  z-index: 9;
+  background-color: #f1f1f1;
+  text-align: center;
+  
+}
+
+.mydivheader {
+  padding: 10px;
+  cursor: move;
+  z-index: 10;
+  background-color: #500000;
+  color: #FFFFFF;
+}
+
+.mainbox{
+    height: 300px;
+    width: 500px;
+    border: 1px solid black;
+    border-radius: 10px;
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+}
+</style>

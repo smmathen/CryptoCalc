5b014e4e8270c2774c1be5fc6081831d6ab6ce9f
Unit404 <jeremydspotts@gmail.com>
2022-01-30 00:21:50 -0600

    added prototype logic to server

--- crypto-calc/src/components/FastPower.vue
+++ crypto-calc/src/components/FastPower.vue
@@ -1,19 +1,19 @@
 <template>
-	<div style="display: block">
-	<form style="display: block;">
 	<div>
-	<label for="base">Base:</label>
-	<input type="number" v-model="base" name="base"/>
+	<form>
+		<div>
+			<label for="base">Base:</label>
+			<input type="number" v-model="base" name="base"/>
 		</div>
-	<div>
-	<label for="exp">Exp:</label>
-	<input type="number" v-model="exp" name="exp"/>
-	</div>
-	<div>
-	<label for="mod">N:</label>
-	<input type="number" v-model="mod" name="exp"/>
-	</div>
-	</form>
+		<div>
+			<label for="exp">Exp:</label>
+			<input type="number" v-model="exp" name="exp"/>
+		</div>
+		<div>
+			<label for="mod">N:</label>
+			<input type="number" v-model="mod" name="exp"/>
+		</div>
+		</form>
 	</div>
 	<div>{{base !== "" ? base : "base"}}<sup>{{exp !== "" ? exp : "exp"}}</sup> mod {{mod !== "" ? mod : "N"}} = {{ans !== "" ? ans : "?"}}</div>
 </template>

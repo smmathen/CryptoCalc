bc8c0ecc65cea58645f50a2425a0aeac2c37e9b5
smmathen <70829009+smmathen@users.noreply.github.com>
2022-01-30 11:15:15 -0600

    combining backend and front end to work together

--- crypto-calc/src/components/FastPower.vue
+++ crypto-calc/src/components/FastPower.vue
@@ -1,21 +1,27 @@
-<template>
+	<template>
 	<div>
 	<form>
-		<div>
-			<label for="base">Base:</label>
+		<div class="equations">
+			<label for="base">Base: </label>
 			<input type="number" v-model="base" name="base"/>
 		</div>
+		<br/>
 		<div>
-			<label for="exp">Exp:</label>
+			<label for="exp">Exp: </label>
 			<input type="number" v-model="exp" name="exp"/>
 		</div>
+		<br/>
 		<div>
-			<label for="mod">N:</label>
+			<label for="mod">N: </label>
 			<input type="number" v-model="mod" name="exp"/>
 		</div>
+		<br/>
+		<br/>
 		</form>
 	</div>
-	<div>{{base !== "" ? base : "base"}}<sup>{{exp !== "" ? exp : "exp"}}</sup> mod {{mod !== "" ? mod : "N"}} = {{ans !== "" ? ans : "?"}}</div>
+	<div>{{base !== "" ? base : "base"}}<sup>{{exp !== "" ? exp : "exp"}}</sup> mod {{mod !== "" ? mod : "N"}} = {{ans !== "" ? ans : "?"}}<br/> 
+	<div>{{solve(base, exp, mod)}}</div>
+	</div>
 </template>
 
 <script>
@@ -35,11 +41,24 @@ export default {
 	},
   },
 	
+	methods:{
+		solve: function(base, exp, mod){
+			return base ** exp % mod;
+		}
+	}
 
 }
 </script>
 
 <style>
+	.equations{
+		margin-top: 30px;
+	}
+
+	#input[type=number]{
+		margin-bottom: 30px;
+	}
+	
 
 </style>
 

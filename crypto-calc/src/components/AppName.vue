cedf7f881985f81ca8c9038faf32037e31775f20
Unit404 <jeremydspotts@gmail.com>
2022-01-29 20:12:07 -0600

    Added FastPower component

--- crypto-calc/src/components/AppName.vue
+++ crypto-calc/src/components/AppName.vue
@@ -1,39 +1,20 @@
 
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+	<h1>{{ msg }}</h1>
   </div>
-  <div>
-<form>
-<label for="base">Base:</label>
-<input type="number" v-model="base" name="base"/>
-<label for="exp">Exp:</label>
-<input type="number" v-model="exp" name="exp"/>
-<label for="mod">N:</label>
-<input type="number" v-model="mod" name="exp"/>
-</form>
-  </div>
-  <div>{{base !== "" ? base : "base"}}<sup>{{exp !== "" ? exp : "exp"}}</sup> mod {{mod !== "" ? mod : "N"}} = {{ans !== "" ? ans : "?"}}</div>
+  <fast-power/>
 </template>
 
 <script>
-export default {
-  name: 'AppName',
+import FastPower from "./FastPower.vue";
+export default{
+  components: { FastPower },
+  name: "AppName",
   props: {
-    msg: String
-  },
-  data: () => ({
-    base: "",
-    exp: "",
-    mod: "",
-    ans: ""
-  }),
-  watch: {
-    base: function () {
-      console.log(this.base);
-    }
+	msg: String,
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
